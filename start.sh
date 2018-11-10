bd9adeeac7fd9e91f704303b9b253377f971b108
David Zemon <david.zemon@redlion.net>
2018-11-09 21:55:54 -0600

    Conan made some annoying changes in v1.9 that require more updates

--- start.sh
+++ start.sh
@@ -3,11 +3,13 @@
 # Note: set user using `--env uid=XXXX --env gid=XXXX`, instead of using
 # docker's `--user` flag
 
-if [ ! -s "${HOME}/.conan/registry.txt" ] ; then
-    cat "${HOME}/.conan/registry.template.txt" >> "${HOME}/.conan/registry.txt"
+if [[ ! -s "${HOME}/.conan/registry.json" ]] ; then
+    conan remote add ci "https://artifactory.redlion.net/artifactory/api/conan/conan-local"
+    conan remote add conan-center "https://conan.bintray.com"
+    conan remote add bintray-community "https://api.bintray.com/conan/conan-community/conan"
 fi
 
-if [ "${uid}" -a "${gid}" ] ; then
+if [[ "${uid}" && "${gid}" ]] ; then
     set -e
     if ! grep --quiet ":${gid}:" /etc/group; then
         groupadd --gid "${gid}" cocaptain
@@ -24,7 +26,7 @@ if [ "${uid}" -a "${gid}" ] ; then
         chown ${uid}:${gid} "${HOME}"
         chown ${uid}:${gid} "${HOME}/.ssh"
     fi
-    chown ${uid}:${gid} "${HOME}/.conan/registry.txt"
+    chown ${uid}:${gid} "${HOME}/.conan/registry.json"
     su_cmd="sudo --preserve-env --user #${uid} --group #${gid}"
     set +e
 fi
